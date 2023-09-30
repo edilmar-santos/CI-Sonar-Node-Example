8477336c24797e11c4cbcd0fa483aedf00adf6b1
edilmar.nascimento <edilmar.nascimento@ab-inbev.com>
2023-09-30 19:36:43 -0300

    test:
    remove comment line from unit test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
 const isEvenOrOdd  = require("../src/index.js")
 
-// test('Should return error message when is not a number', () => {
-//     expect(isEvenOrOdd("a")).toBe("Error: the parameter is not a number");
-// })
+test('Should return error message when is not a number', () => {
+    expect(isEvenOrOdd("a")).toBe("Error: the parameter is not a number");
+})
 
 test('Should return EVEN', () => {
     expect(isEvenOrOdd(2)).toBe("Even");
